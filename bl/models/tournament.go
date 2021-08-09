aab719c35d529f9c520782b7da3ef3bf4ed1796c
Artyom Filippenkov <filippenkov95@gmail.com>
2021-08-09 12:57:39 +0300

    added status field for tournament model

--- bl/models/tournament.go
+++ bl/models/tournament.go
@@ -4,11 +4,20 @@ import (
 	"github.com/google/uuid"
 )
 
+type TournamentStatus string
+
+const (
+	Active TournamentStatus = "Active"
+	Cancel TournamentStatus = "Cancel"
+	Finish TournamentStatus = "Finish"
+)
+
 type Tournament struct {
-	ID      uuid.UUID `"sql:", type:uuid"`
+	ID      uuid.UUID `sql:", type:uuid"`
 	Name    string
 	Deposit float64
 	Prize   float64
 	Users   []User
 	Winner  string
+	Status  TournamentStatus
 }

6d5cd4a3e4094374e7cf330146e0f4be1e44d7f5
artyom <filippenkov95@gmail.com>
2021-08-10 15:07:14 +0300

    put func for generate winner in usecase and added func for get random user of tournament

--- bl/usecases/tournament-repo.go
+++ bl/usecases/tournament-repo.go
@@ -7,11 +7,15 @@ import (
 
 type TournamentRepository interface {
 	Create(tournament *models.Tournament) (uuid.UUID, error)
+
 	GetByID(id uuid.UUID) (*models.Tournament, error)
-	AddUserToTournament(tournamentID uuid.UUID, userID uuid.UUID) error
+	GetRandomUserOfTournament(tournamentID uuid.UUID) (*models.User, error)
+
+	AddUserToTournament(tournamentID, userID uuid.UUID) error
 	AddToPrize(ID uuid.UUID, addend float64) error
-	GetWinner(tournamentID uuid.UUID) (*models.User, error)
 	AddDepositToUsersOfTournament(tournamentID uuid.UUID) error
+	AddWinner(tournamentID, userID uuid.UUID) error
+
 	// TODO: added this func after merge interfaces
 	//	ChangeStatus(tournamentID uuid.UUID, newStatus )
 

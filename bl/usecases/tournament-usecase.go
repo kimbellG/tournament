e00494264da097b7fdafbace9d9aeb7132f08f28
artyom <filippenkov95@gmail.com>
2021-08-10 12:34:29 +0300

    added tournament usecase interface

---
+++ bl/usecases/tournament-usecase.go
@@ -0,0 +1,14 @@
+package usecases
+
+import (
+	"github.com/google/uuid"
+	"github.com/kimbellG/tournament-bl/models"
+)
+
+type TournamentUsecase interface {
+	Create(tournament *models.Tournament) (uuid.UUID, error)
+	GetByID(id uuid.UUID) (*models.Tournament, error)
+	Join(tournamnetID uuid.UUID, userID uuid.UUID) error
+	Finish(id uuid.UUID) error
+	Cancel(id uuid.UUID) error
+}

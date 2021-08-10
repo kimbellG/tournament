d50efe3bfd7f0cdb62a867368193f7e520635d45
artyom <filippenkov95@gmail.com>
2021-08-10 13:53:22 +0300

    renamed update-balance function

--- bl/usecases/tournament-repo.go
+++ bl/usecases/tournament-repo.go
@@ -1,13 +1,16 @@
 package usecases
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/kimbellG/tournament-bl/models"
+)
 
 type TournamentRepository interface {
 	Create(tournament *models.Tournament) (uuid.UUID, error)
 	GetByID(id uuid.UUID) (*models.Tournament, error)
 	AddUserToTournament(tournamentID uuid.UUID, userID uuid.UUID) error
-	ChangePrice(ID uuid.UUID, addend float64) error
-	GetWinner(tournamentID uuid.UUID) (*models.Users, error)
+	AddToPrize(ID uuid.UUID, addend float64) error
+	GetWinner(tournamentID uuid.UUID) (*models.User, error)
 	AddDepositToUsersOfTournament(tournamentID uuid.UUID) error
 	// TODO: added this func after merge interfaces
 	//	ChangeStatus(tournamentID uuid.UUID, newStatus )

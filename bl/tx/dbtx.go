b880a1a91ccb3402b45a54264f911f9621310fc8
artyom <filippenkov95@gmail.com>
2021-08-11 15:40:45 +0300

    added wrapper for transactions

--- bl/tx/dbtx.go
+++ bl/tx/dbtx.go
@@ -1,10 +1,45 @@
 package tx
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/kimbellG/kerror"
+)
 
 type DBTX interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
 	Prepare(query string) (*sql.Stmt, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
+type TransactionFunction func(store DBTX) error
+
+type Transactioner interface {
+	WithTransaction(fn TransactionFunction) error
+}
+
+type Store struct {
+	db *sql.DB
+}
+
+func (s *Store) WithTransaction(fn TransactionFunction) error {
+	tx, err := s.db.Begin()
+	if err != nil {
+		return kerror.Newf(kerror.SQLTransactionBeginError, "begin transaction: %w", err)
+	}
+
+	if err := fn(tx); err != nil {
+		if rbErr := tx.Rollback(); err != nil {
+			return kerror.Errorf(err, "transaction roolback error: %w; transaction error", rbErr)
+		}
+
+		return kerror.Errorf(err, "transaction function error")
+	}
+
+	if err := tx.Commit(); err != nil {
+		return kerror.Newf(kerror.SQLTransactionCommitError, "transaction commit: %w", err)
+	}
+
+	return nil
 }

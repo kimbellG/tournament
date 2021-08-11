5eb372ecd9cd4f566e383dce0b40c1f970827f2d
artyom <filippenkov95@gmail.com>
2021-08-11 12:35:05 +0300

    changed repo interface for controller
    added in parametr of each func interface tx.DBTX for implementaion
    transact business logic

--- bl/usecases/tournament-repo.go
+++ bl/usecases/tournament-repo.go
@@ -3,18 +3,19 @@ package usecases
 import (
 	"github.com/google/uuid"
 	"github.com/kimbellG/tournament-bl/models"
+	"github.com/kimbellG/tournament-bl/tx"
 )
 
 type TournamentRepository interface {
-	Create(tournament *models.Tournament) (uuid.UUID, error)
+	Create(repo tx.DBTX, tournament *models.Tournament) (uuid.UUID, error)
 
-	GetByID(id uuid.UUID) (*models.Tournament, error)
-	GetRandomUserOfTournament(tournamentID uuid.UUID) (*models.User, error)
+	GetByID(repo tx.DBTX, id uuid.UUID) (*models.Tournament, error)
+	GetRandomUserOfTournament(repo tx.DBTX, tournamentID uuid.UUID) (*models.User, error)
 
-	AddUserToTournament(tournamentID, userID uuid.UUID) error
-	AddToPrize(ID uuid.UUID, addend float64) error
-	AddDepositToUsersOfTournament(tournamentID uuid.UUID) error
-	AddWinner(tournamentID, userID uuid.UUID) error
+	AddUserToTournament(repo tx.DBTX, tournamentID, userID uuid.UUID) error
+	AddToPrize(repo tx.DBTX, ID uuid.UUID, addend float64) error
+	AddDepositToUsersOfTournament(repo tx.DBTX, tournamentID uuid.UUID) error
+	AddWinner(repo tx.DBTX, tournamentID, userID uuid.UUID) error
 
-	ChangeStatus(tournamentID uuid.UUID, newStatus models.TournamentStatus) error
+	ChangeStatus(repo tx.DBTX, tournamentID uuid.UUID, newStatus models.TournamentStatus) error
 }

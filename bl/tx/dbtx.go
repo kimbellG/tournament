6f7ef7b5204c13557c6464e96d9575a6aba76645
artyom <filippenkov95@gmail.com>
2021-08-11 11:33:58 +0300

    added tx package with dbtx interface for implementain transaction

---
+++ bl/tx/dbtx.go
@@ -0,0 +1,10 @@
+package tx
+
+import "database/sql"
+
+type DBTX interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Prepare(query string) (*sql.Stmt, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+}

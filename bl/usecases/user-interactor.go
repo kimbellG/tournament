989363cde9e5a76dc78e93d6f5b7dd91a09097ff
artyom <filippenkov95@gmail.com>
2021-08-11 16:58:44 +0300

    added transactive implemenatation of business logic

--- bl/usecases/user-interactor.go
+++ bl/usecases/user-interactor.go
@@ -4,41 +4,79 @@ import (
 	"github.com/google/uuid"
 	"github.com/kimbellG/kerror"
 	"github.com/kimbellG/tournament-bl/models"
+	"github.com/kimbellG/tournament-bl/tx"
 )
 
 type UserInteractor struct {
 	UserRepo UserRepository
+	store    tx.Transactioner
 }
 
 func (ui *UserInteractor) Save(user *models.User) (uuid.UUID, error) {
-	id, err := ui.UserRepo.Save(user)
+	var id uuid.UUID
+
+	err := ui.store.WithTransaction(func(store tx.DBTX) error {
+		var err error
+
+		id, err = ui.UserRepo.Insert(store, user)
+		if err != nil {
+			return kerror.Errorf(err, "repository")
+		}
+
+		return nil
+	})
 	if err != nil {
-		return id, kerror.Errorf(err, "repository")
+		return id, kerror.Errorf(err, "execution transactive")
 	}
 
 	return id, nil
 }
 
 func (ui *UserInteractor) GetByID(id uuid.UUID) (*models.User, error) {
-	user, err := ui.UserRepo.GetByID(id)
+	var user *models.User
+
+	err := ui.store.WithTransaction(func(store tx.DBTX) error {
+		var err error
+
+		user, err = ui.UserRepo.SelectByID(store, id)
+		if err != nil {
+			return kerror.Errorf(err, "repository")
+		}
+
+		return nil
+	})
 	if err != nil {
-		return nil, kerror.Errorf(err, "repository")
+		return nil, kerror.Errorf(err, "execution transactive")
 	}
 
 	return user, nil
 }
 
 func (ui *UserInteractor) DeleteByID(id uuid.UUID) error {
-	if err := ui.UserRepo.DeleteByID(id); err != nil {
-		return kerror.Errorf(err, "repository")
+	err := ui.store.WithTransaction(func(store tx.DBTX) error {
+		if err := ui.UserRepo.DeleteByID(store, id); err != nil {
+			return kerror.Errorf(err, "repository")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return kerror.Errorf(err, "execution transaction")
 	}
 
 	return nil
 }
 
 func (ui *UserInteractor) UpdateBalance(id uuid.UUID, addend float64) error {
-	if err := ui.UserRepo.SumToBalance(id, addend); err != nil {
-		return kerror.Errorf(err, "repository")
+	err := ui.store.WithTransaction(func(store tx.DBTX) error {
+		if err := ui.UserRepo.UpdateBalanceBySum(store, id, addend); err != nil {
+			return kerror.Errorf(err, "repository")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return kerror.Errorf(err, "execution transaction")
 	}
 
 	return nil

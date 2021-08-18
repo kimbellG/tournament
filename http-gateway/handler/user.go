597ca6d2c1ab4dd5a6f15bcea26045303c6cc5c8
artyom <filippenkov95@gmail.com>
2021-08-18 15:08:52 +0300

    added user handler functions for model

---
+++ http-gateway/handler/user.go
@@ -0,0 +1,103 @@
+package handler
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kimbellG/tournament/http/internal"
+)
+
+type CreateUserResponse struct {
+	ID string `json:"id"`
+}
+
+func Close(cl io.Closer) {
+	if err := cl.Close(); err != nil {
+		log.Println(err)
+	}
+}
+
+func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	user := &internal.User{}
+	if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+		http.Error(w, "Failed to marshal json request for creating user", http.StatusBadRequest)
+		return
+	}
+	defer Close(r.Body)
+
+	id, err := h.tournament.CreateUser(r.Context(), user)
+	if err != nil {
+		http.Error(w, "Failed to create user:"+err.Error(), parsing)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(CreateUserResponse{id}); err != nil {
+		http.Error(w, "Failed to encode answer in body: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type UserRequest struct {
+	ID string `json:"id"`
+}
+
+func (h *Handler) GetUserByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)
+
+	user, err := h.tournament.GetUserByID(r.Context(), id[idPath])
+	if err != nil {
+		http.Error(w, "Failed to get user by id: "+err.Error(), parsing)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Failed to encode user in body: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *Handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)
+
+	if err := h.tournament.DeleteUser(r.Context(), id[idPath]); err != nil {
+		http.Error(w, "Failed to delete user: "+err.Error(), parsing)
+		return
+	}
+}
+
+type UpdateBalanceRequest struct {
+	Summand float64 `json:"summand"`
+}
+
+func (h *Handler) AddToBalance(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)[idPath]
+
+	updateRequest := &UpdateBalanceRequest{}
+	if err := json.NewDecoder(r.Body).Decode(updateRequest); err != nil {
+		http.Error(w, "Failed to decode update request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.tournament.UpdateBalanceBySum(r.Context(), id, updateRequest.Summand); err != nil {
+		http.Error(w, "Failed to add points to user balance: "+err.Error(), parsing)
+		return
+	}
+}
+
+func (h *Handler) TakeFromBalance(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)[idPath]
+
+	takeRequest := &UpdateBalanceRequest{}
+	if err := json.NewDecoder(r.Body).Decode(takeRequest); err != nil {
+		http.Error(w, "Failed to decode take request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.tournament.UpdateBalanceBySum(r.Context(), id, -takeRequest.Summand); err != nil {
+		http.Error(w, "Failed to take points from user balance: "+err.Error(), parsing)
+		return
+	}
+}

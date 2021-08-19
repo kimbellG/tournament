9ae03df557dac746694c6ed6dd091c43f2c99a0a
artyom <filippenkov95@gmail.com>
2021-08-19 15:39:54 +0300

    added function for registration endpoints

--- http-gateway/go.mod
+++ http-gateway/go.mod
@@ -3,8 +3,9 @@ module github.com/kimbellG/tournament/http
 go 1.16
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 	github.com/kimbellG/kerror v0.0.0-20210819100523-8eb79808c2bd
 	google.golang.org/genproto v0.0.0-20210818220304-27ea9cc85d9f // indirect
 	google.golang.org/grpc v1.40.0
--- http-gateway/handler/registry.go
+++ http-gateway/handler/registry.go
@@ -1,3 +1,16 @@
 package handler
 
+import (
+	"github.com/gorilla/mux"
+	"github.com/kimbellG/tournament/http/controller"
+)
+
 const idPath = "id"
+
+func RegisterUserEndpoints(router *mux.Router, tc controller.TournamentController) {
+	h := NewHandler(tc)
+
+	router.HandleFunc("/", h.CreateUser).Methods("POST")
+	router.HandleFunc("/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}", h.GetUserByID).Methods("GET")
+	router.HandleFunc("/{id:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}", h.DeleteUser).Methods("DELETE")
+}

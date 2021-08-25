b4274775ad385181fed268f48a6f77250d7a01e7
Артём <filippenkov95@gmail.com>
2021-08-25 12:13:08 +0300

    added error handling for controller

--- http-gateway/controller/error-handler.go
+++ http-gateway/controller/error-handler.go
@@ -15,13 +15,30 @@ var grpcToErrorCode = map[codes.Code]kerror.StatusCode{
 	codes.Unknown:            kerror.Unknown,
 }
 
-func decodeGrpcError(err error) error {
+func decodeErrorFromGrpc(err error) error {
 	gerr, ok := status.FromError(err)
 	if !ok {
 		return kerror.New(err, kerror.Unknown)
 	}
 
+	for _, d := range gerr.Details() {
+		switch info := d.(type) {
+		case errorpb.ErrorHandler:
+			return errorFromProto(info)
+		default:
+		}
+	}
+
 	return kerror.New(gerr.Err(), UnmarshalStatusCode(gerr.Code()))
+}
+
+func errorFromProto(err errorpb.ErrorHandler) error {
+	switch e := err.Error.(type) {
+	case *errorpb.ErrorHandler_Kerror:
+		return kerror.New(errors.New(e.Kerror.GetMsg()), kerror.StatusCode(e.Kerror.GetCode()))
+	default:
+		return kerror.New(errors.New("unexcepted error"), kerror.Unknown)
+	}
 }
 
 func UnmarshalStatusCode(code codes.Code) kerror.StatusCode {
